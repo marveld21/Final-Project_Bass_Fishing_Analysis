8790a22065eb59dff6896cdfc0c4f7c2f929c98a
marveld21 <derekdenney21@gmail.com>
2022-11-03 16:06:07 -0500

    Working Logistic Regression Model

--- model0.2.py
+++ model0.2.py
@@ -35,9 +35,11 @@ df.tail()
 
 df.nunique()
 #drop not needed columns
-fish_df = df.drop(['Angler_Name','Species','Weight','Recorded_Time','Tournament_Day','Weight_Group','Recorded_Time_24','Recorded_Time_hour'],axis=1)
+fish_df = df.drop(['Angler_Name','Species','Weight','Recorded_Time','Tournament_Day','Weight_Group','Recorded_Time_24','Recorded_Time_hour','Weight-lbs'],axis=1)
 # attempt model using Bait to predict Weight Group
 fish_df.columns
+
+
 
 #simplify bait column ----binning
 bait_count = fish_df.Bait.value_counts()
@@ -74,33 +76,57 @@ fish_df.Cover.value_counts()
 
 fish_df.nunique()
 
+
+#split target and features
+fish_df_features = fish_df.drop(['Fish_Size'],axis=1)
+fish_df_target = fish_df['Fish_Size']
+fish_df_target
+fish_df_features
+
+
 #encode categorical variables
 # Generate our categorical variable lists
-fish_cat = fish_df.dtypes[fish_df.dtypes == "object"].index.tolist()
+fish_cat = fish_df_features.dtypes[fish_df_features.dtypes == "object"].index.tolist()
 fish_cat
+
+
+fish_df_features[fish_cat]
 
 # Create a OneHotEncoder instance
 enc = OneHotEncoder(sparse=False)
 
 # Fit and transform the OneHotEncoder using the categorical variable list
-encode_df = pd.DataFrame(enc.fit_transform(fish_df[fish_cat]))
+encode_df = pd.DataFrame(enc.fit_transform(fish_df_features[fish_cat]))
 
 # Add the encoded variable names to the dataframe
 encode_df.columns = enc.get_feature_names(fish_cat)
 encode_df
 
 
+
+#simple encode for target
+target_encode_df = fish_df_target.replace(('Big Fish', 'Small Fish'), (1, 0))
+target_encode_df.head()
+
+
+
+
+
+
+
+
+
 # Merge one-hot encoded features and drop the originals
-fish_df= fish_df.merge(encode_df,left_index=True,right_index=True)
-fish_df = fish_df.drop(columns = fish_cat)
-fish_df['Weight-lbs']
+# fish_df= fish_df.merge(encode_df,left_index=True,right_index=True)
+# fish_df = fish_df.drop(columns = fish_cat)
+# fish_df['Fish_Size']
 
 
 #all of the data is catagorical
 
 # Split our preprocessed data into our features and target arrays
-y = fish_df['Weight-lbs']
-X = fish_df.drop(columns="Weight-lbs")
+y = target_encode_df
+X = encode_df
 
 # Split the preprocessed data into a training and testing dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
@@ -117,30 +143,26 @@ X_train_scaled = X_scaler.transform(X_train)
 X_test_scaled = X_scaler.transform(X_test)
 
 
+X_train_scaled.shape
+y_train.shape
+
 #### logistical regression
 
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(solver='lbfgs',
+                                max_iter=200,
+                                random_state=1)
+
+classifier.fit(X_train, y_train)
 
 
+y_pred = classifier.predict(X_test)
+results = pd.DataFrame({"Prediction": y_pred, "Actual": y_test}).reset_index(drop=True)
+results.head(20)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
 
 
 
